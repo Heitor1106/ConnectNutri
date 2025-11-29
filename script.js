932b1c7d87da1e1bb1ea7baf76f58916c87fba2c
Odaviespecifico <jak_singer@hotmail.com>
2025-11-29 18:54:56 -0300

    Added basic integration with supabase backend

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('form');
-    const passwordInput = document.querySelector('input[name="senha"]');
-    const togglePassword = document.querySelector('#togglePassword');
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+const supabaseUrl = 'https://rrurrbxgmqvcoqrdoyxt.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJydXJyYnhnbXF2Y29xcmRveXh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0NDQ3MDEsImV4cCI6MjA4MDAyMDcwMX0.lnEF038G1I6ihIA8dyomdjpxB5WigJXIYyO9ruwJ7QE'
+const supabase = createClient(supabaseUrl, supabaseKey)
 
+// Quando o dom carregar
+document.addEventListener('DOMContentLoaded', () => {
     // --- Funcionalidade para mostrar/ocultar senha ---
+    handleTogglePassword()
+
+    // --- Validação básica do formulário no envio ---
+    validateForm()
+
+    getPatients()
+});
+
+function handleTogglePassword() {
+    const togglePassword = document.querySelector('#togglePassword');
+    const passwordInput = document.querySelector('input[name="senha"]');
     if (togglePassword && passwordInput) {
         togglePassword.addEventListener('click', () => {
             // Adiciona uma classe para animar o ícone e a remove após a animação
@@ -26,8 +39,11 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }
+}
 
-    // --- Validação básica do formulário no envio ---
+function validateForm() {
+    const form = document.querySelector('form');
+    const passwordInput = document.querySelector('input[name="senha"]');
     if (form) {
         form.addEventListener('submit', (event) => {
             event.preventDefault(); // Impede o envio do formulário para validação
@@ -44,4 +60,21 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }
-});
+}
+
+async function getPatients() {
+    let { data, error } = await supabase
+        .from('Pacientes')
+        .select('*')
+    console.log(data)
+
+    // const body = document.querySelector('body')
+    // const div = document.createElement('div')
+    // div.innerHTML = `
+    //     <h1>Cliente</h1>
+    //     <p>Nome: ${data[0].nome}</p>
+    //     <p>endereço: ${data[0].endereço}</p>
+    //     <p>Email: ${data[0].cpf}</p>
+    // `
+    // body.appendChild(div)
+}
